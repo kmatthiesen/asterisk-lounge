43d81668b4c9ae1483462657bc1bcc97a5d262cf
mpKness <mKness@catalystitservices.com>
2015-11-11 21:05:32 -0800

    worked on getting the views to change, currently wont work because i need to set up the localhost thing

--- JS_al.js
+++ JS_al.js
@@ -1,4 +1,25 @@
-var app = angualar.module('myApp', []);
-app.controller('blogCtrl', function($scope) {
-	
+var alApp = angular.module('alApp', ['ngRoute']);
+
+alApp.config(function($routeProvider) {
+    $routeProvider
+    
+    // route for the home page
+    .when('/', {
+	templateUrl : 'pages/home.html',
+	controller  : 'mainController'
+    })
+
+    // route for the records page
+    .when('/records', {
+	templateUrl : 'pages/records.html',
+	controller  : 'recordsController'
+    })
+});
+
+alApp.controller('mainController', function($scope) {
+    $scope.message = 'EVERYONE COME AND SEE HOW GOOD I LOOK!!!';
+});
+
+alApp.controller('recordsController', function($scope) {
+    $scope.message = 'MAC IS A GOOBER!!!';
 });
