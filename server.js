c3f1ed65259a650a1d907565ac258a59571dfa51
mpKness <mKness@catalystitservices.com>
2015-12-15 18:35:48 -0800

    update package.json, added node depenencies, wrote simple express server, reorganized file structure

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
-var connect = require('connect');
+/*var connect = require('connect'); // dumb static server in case i break everything
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(8080);*/
 
+// Depenencies
+var http = require('http');
+var express = require('express');
+var app = express();
+//var mongoose = require('mongoose'); // connects to mongodb
+var morgan = require('morgan');  // log requests to console
+var bodyParser = require('body-parser'); // pull information from html POST
+var methodOverride = require('method-override'); // simpulate DELETE and PUT
+
+// TODO connect to mongodb with mongoose
+
+// Build Server
+app.use(express.static(__dirname + '/public'));  // set static file location
+app.use(morgan('dev'));  // log requests to console
+app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
+
+// listen (start app with node server.js)
+app.listen(8080);
+console.log("App listening on port 8080");
