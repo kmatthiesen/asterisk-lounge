d0ddc2d61cf5b3fea79cb5d8454484d70725b56b
mpKness <mKness@catalystitservices.com>
2015-12-28 11:10:51 -0800

    added 5tribes model to backend

---
+++ public/js/models/5tribes.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'); // may need to change this since i now have multiple models
+var Schema = mongoose.Schema;
+
+var 5tribesSchema = new Schema({
+  date: String,
+  gold: Number,
+  yellow: Number,
+  white: Number,
+  djinn: Number,
+  camel: Number,
+  palm: Number,
+  palace: Number,
+  resouece: Number,
+  total: Number
+});
+
+module.exports = mongoose.model('5tribes', 5tribesSchema);
